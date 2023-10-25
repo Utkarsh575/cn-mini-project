ee5282f84a99e54459ec1b3dbedd2d38dc22ff7a
utkarsh575 <utkarshlohani575@gmail.com>
2023-10-26 01:31:37 +0530

    :art: styled client

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,118 +1,143 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { CodeBlock, atomOneDark, dracula } from "react-code-blocks";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
 export default function Home() {
+  const [comments, setComments] = useState([]);
+
+  const [showAuthor, setShowAuthor] = useState<boolean>(false);
+  // const fetchData = async () => {
+  //   const url: RequestInfo | URL =
+  //     "https://jsonplaceholder.typicode.com/posts/1/comments";
+  //   try {
+  //     // fetch data
+  //     let res = await fetch(url);
+  //     res = await res.json();
+  //     console.log(res);
+  //     setComments(res);
+  //   } catch (e) {
+  //     //error part
+  //     console.log(e);
+  //   }
+  // };
+
+  const MY_QUERY = gql`
+    ${showAuthor ? "{books{id name}}" : "{books{id name, author{name}}}"}
+  `;
+  const { loading, error, data } = useQuery(MY_QUERY);
+  console.log(data);
+
+  const [restBooks, setRestBooks] = useState([]);
+
+  const fetchBooksViaRest = async () => {
+    let res = await fetch("http://localhost:5000/rest");
+    let data = await res.json();
+    console.log(data);
+    setRestBooks(data);
+  };
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [data, showAuthor, restBooks]);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-row items-start justify-center w-full p-10 bg-black h-full`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      <div className="flex justify-between items-start  w-full h-full ">
+        <div className="flex flex-col bg-gray-900 items-center justify-center w-[50%] h-full p-10 rounded-l-xl">
+          <h1 className="font-bold text-4xl py-2 text-white">
+            GraphQL response
+          </h1>
+          <div className="flex gap-5 mb-2 ">
+            <button
+              className="px-7 rounded-md py-3 bg-purple-400 shadow-lg hover:bg-purple-500 hover:transition-colors hover:duration-500"
+              onClick={() => {
+                setShowAuthor(!showAuthor);
+              }}
+            >
+              show Author
+            </button>
+            <button
+              className="px-7 rounded-md py-3 bg-purple-400 shadow-lg hover:bg-purple-500 hover:transition-colors hover:duration-500"
+              onClick={() => {
+                setShowAuthor(!showAuthor);
+              }}
+            >
+              Don't show Author
+            </button>
+          </div>
+          <div className="flex flex-wrap justify-center items-center gap-2">
+            {!loading &&
+              data?.books.map((e: any) => {
+                return (
+                  <div className="w-[15rem] h-[7rem] bg-yellow-200  border-r-4 border-t-4 border-white rounded-md  font-bold p-2 ">
+                    <h1>📗{e.id}</h1>
+                    <h1>{e.name}</h1>
+                    <h1>{e?.author?.name}</h1>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="w-full mt-2 h-[15rem]">
+            <CodeBlock
+              text={JSON.stringify(data)}
+              language="javascript"
+              showLineNumbers={true}
+              wrapLongLines={true}
+              theme={atomOneDark}
             />
-          </a>
+          </div>
+        </div>
+        <div className=" h-full w-[50%] flex flex-col items-center justify-center p-10 bg-slate-800 rounded-r-xl">
+          <h1 className="text-4xl font-bold py-2 text-white">
+            REST API RESPONSE
+          </h1>
+
+          <div className="flex justify-center w-full items-center gap-5">
+            <button
+              className="px-7 rounded-md py-3 bg-purple-400 shadow-lg hover:bg-purple-500 hover:transition-colors hover:duration-500"
+              onClick={() => {
+                fetchBooksViaRest();
+              }}
+            >
+              Fetch All Books
+            </button>
+            {/* <button className="px-7 rounded-md py-3 bg-purple-400 shadow-lg hover:bg-purple-500 hover:transition-colors hover:duration-500">
+              Fetch All Books
+            </button> */}
+            <button
+              className="px-7 rounded-md py-3 bg-purple-400 shadow-lg hover:bg-purple-500 hover:transition-colors hover:duration-500"
+              onClick={() => setRestBooks([])}
+            >
+              CLEAR
+            </button>
+          </div>
+          <div className="flex flex-wrap justify-center items-center gap-2 mt-2">
+            {restBooks.map((e: any) => {
+              return (
+                <div className="w-[15rem] h-[7rem] bg-green-300  border-r-4 border-t-4 border-white rounded-md  font-bold p-2 ">
+                  <h1>📕{e.id}</h1>
+                  <h1>{e.name}</h1>
+                  <h1>{e?.author?.name}</h1>
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-full mt-2 h-[15rem]">
+            <CodeBlock
+              text={JSON.stringify(restBooks)}
+              language="javascript"
+              showLineNumbers={true}
+              wrapLongLines={true}
+              theme={dracula}
+            />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
